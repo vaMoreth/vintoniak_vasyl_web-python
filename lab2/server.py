1d167af2149ece49afd14d974b6ca174c6a35666
vaMoreth <jummyvasya24@gmail.com>
2023-10-12 12:52:44 +0200

    add server and another files

--- lab2/server.py
+++ lab2/server.py
@@ -2,4 +2,5 @@ from http.server import  HTTPServer, CGIHTTPRequestHandler
 adders = ("localhost", 5050)
 
 server = HTTPServer(adders, CGIHTTPRequestHandler)
+print("Listenint...")
 server.serve_forever()
