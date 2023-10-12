df157e67558aae8ac63ade94383198aff6cd5730
vaMoreth <jummyvasya24@gmail.com>
2023-10-12 13:43:17 +0200

    modify process_form.py adding cookies checker

--- lab2/cgi-bin/process_form.py
+++ lab2/cgi-bin/process_form.py
@@ -1,13 +1,40 @@
 import cgi
+import http.cookies
+import os
 
+def get_cookie_value(cookie, key):
+    cookies = http.cookies.SimpleCookie(cookie)
+    return cookies.get(key)
+
+def set_cookie(key, value):
+    cookie = http.cookies.SimpleCookie()
+    cookie[key] = value
+    print(cookie)
+
+def delete_cookie(key):
+    cookie = http.cookies.SimpleCookie()
+    cookie[key] = ""
+    cookie[key]['expires'] = 0
+    print(cookie)
+
+cookie_string = os.environ.get('HTTP_COOKIE', '')
+form_counter_cookie = get_cookie_value(cookie_string, 'form_counter')
+
+form_counter = int(form_counter_cookie.value) if form_counter_cookie else 0
+form_counter += 1
+
+set_cookie('form_counter', form_counter)
 print("Content-type: text/html\n")
 
 form = cgi.FieldStorage()
-
 name = form.getvalue("name")
 gender = form.getvalue("gender")
 favorite_food = form.getlist("food")
 favorite_city = form.getvalue("city")
+
+if form.getvalue('delete_cookies'):
+    delete_cookie('form_counter')
+    form_counter = 0
 
 print("<html>")
 print("<head>")
@@ -19,5 +46,9 @@ print(f"<p>Name: {name}</p>")
 print(f"<p>Gender: {gender} </p>")
 print("<p>Favorite Ukrainian Food: " + ", ".join(favorite_food) + "</p>")
 print(f"<p>Favorite Ukrainian City: {favorite_city} </p>")
+print(f"<p>Forms filled out by user: {form_counter}</p>")
+print("<form action='process_form.py' method='post'>")
+print("<input type='submit' value='Delete Cookies' name='delete_cookies'>")
+print("</form>")
 print("</body>")
 print("</html>")
