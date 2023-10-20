32a79808db579ac958e75d733e7f6e005791be7c
vaMoreth <jummyvasya24@gmail.com>
2023-10-20 15:27:09 +0200

    fix some troubles and make new styles

--- lab3/app.py
+++ lab3/app.py
@@ -1,22 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('portfolio.html')
+    os_info = os.name
+    user_agent = request.user_agent.string
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return render_template('portfolio.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
 
 @app.route('/portfolio')
 def portfolio():
-    return render_template('portfolio.html')
+    os_info = os.name
+    user_agent = request.user_agent.string
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return render_template('portfolio.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
 
 @app.route('/skills')
 def skills():
-    return render_template('skills.html')
+    os_info = os.name
+    user_agent = request.user_agent.string
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return render_template('skills.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
 
 @app.route('/resume')
 def resume():
-    return render_template('resume.html')
+    os_info = os.name
+    user_agent = request.user_agent.string
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return render_template('resume.html', os_info=os_info, user_agent=user_agent, current_time=current_time)
 
 if __name__ == '__main__':
     app.run(debug=True)
