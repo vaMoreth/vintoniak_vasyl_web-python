f136b687e9ed94cd95fa033ac27ffd8cba016c5b
vaMoreth <jummyvasya24@gmail.com>
2023-10-22 16:35:53 +0200

    complete task 1

--- lab4/app/run.py
+++ lab4/app/run.py
@@ -1,4 +1,38 @@
-from app import app
+from flask import Flask, render_template, request, redirect, url_for, session
+import json
+
+app = Flask(__name__)
+app.secret_key = b"secret"
+
+with open('users.json', 'r') as f:
+    users = json.load(f)
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username in users and users[username] == password:
+        session['username'] = username
+        return redirect(url_for('info'))
+    else:
+        return "Login failed. Please check your username and password."
+
+@app.route('/info')
+def info():
+    if 'username' in session:
+        return render_template('info.html')
+    else:
+        return "You are not logged in."
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
--- lab4/app/templates/views.py
+++ lab4/app/templates/views.py
@@ -4,3 +4,4 @@ from app import app
 @app.route("/")
 def index():
     return render_template("index.html")
+
