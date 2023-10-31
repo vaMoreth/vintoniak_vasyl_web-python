419dd7f04d58c1e5965a041c2b097b7bc4e2f8bd
vaMoreth <jummyvasya24@gmail.com>
2023-10-31 12:21:27 +0100

    complete task 1 and 2 from lab5

--- lab5/app/forms.py
+++ lab5/app/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=10)])
+    remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
