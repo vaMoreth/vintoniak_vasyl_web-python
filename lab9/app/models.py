d1b3f9cc75daf8cad533c6aa115574d122cb5561
vaMoreth <jummyvasya24@gmail.com>
2023-11-10 12:59:00 +0100

    add photo in account.html

--- lab9/app/models.py
+++ lab9/app/models.py
@@ -16,7 +16,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='static/images/default.jpg')
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
 
     def is_authenticated(self):
